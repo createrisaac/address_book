dbd98323ec027704ee0d6c0ff267b9c97ef86ce6
Samuel <samueltamang482@gmail.com>
2025-10-21 16:28:22 +0545

    add another person's detail if wanted

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+
+struct person
+{
+    string name;
+    string address;
+    int phone;
+};
+
+void addinfo(){
+    person person2;
+    cout<<"Name of the person :";
+    cin>>person2.name;
+    cout<<endl <<"Address :";
+    cin>>person2.address;
+    cout<<endl <<"Phone number :";
+    cin>>person2.phone;
+
+    cout <<"person1 details:" <<endl ;
+    cout<<person2.name <<endl;
+    cout<<person2.address <<endl;
+    cout<<person2.phone <<endl;
+
+}
+
+int main()
+{   person person1;
+    cout<<"Name of the person :";
+    cin>>person1.name;
+    cout<<endl <<"Address :";
+    cin>>person1.address;
+    cout<<endl <<"Phone number :";
+    cin>>person1.phone;
+
+    cout <<"person1 details:" <<endl;
+    cout<<person1.name <<endl;
+    cout<<person1.address <<endl;
+    cout<<person1.phone <<endl;
+
+    char question;
+    cout<<"Do you want add another person's detail?(y/n)";
+    cin>>question;
+    if(question='y'){
+    addinfo();
+}
+
+
+}
