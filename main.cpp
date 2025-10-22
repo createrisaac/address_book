d4d5d9e25cd1f80931f667fd7918d2331c06908f
Samuel <samueltamang482@gmail.com>
2025-10-22 16:28:20 +0545

    final address book

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include<fstream>
+#include<vector>
 #include <string>
 using namespace std;
 
@@ -8,54 +10,73 @@ struct person {
     string phone;
 };
 
-void addinfo() {
-    person person2;
-    cout << "Name of the person: ";
-    cin >> person2.name;
-    cout << "Address: ";
-    cin >> person2.address;
-    cout << "Phone number: ";
-    cin >> person2.phone;
+    vector<person> people;
 
-    cout << "person2 details:" << endl;
-    cout << person2.name << endl;
-    cout << person2.address << endl;
-    cout << person2.phone << endl;
+void addinfo() {
+    person newperson;
+    cout << "Name of the person: ";
+    cin >> newperson.name;
+    cout << "Address: ";
+    cin >> newperson.address;
+    cout << "Phone number: ";
+    cin >> newperson.phone;
+
+    people.push_back(newperson);
+    cout<<"person added successfully.";
 }
 
-int main() {
-    person person1;
+    void showinfo(){
+        cout <<"/n All persons detail.";
+        for(int i=0;i<people.size();i++){
+            cout<<"Person" <<i+1 <<endl;
+            cout<<"Name : " <<people[i].name <<endl;
+            cout<<"Address : " <<people[i].address <<endl;
+            cout<<"Phone : " <<people[i].phone <<endl;
 
-    cout << "Name of the person: ";
-    cin >> person1.name;
-    cout << "Address: ";
-    cin >> person1.address;
-    cout << "Phone number: ";
-    cin >> person1.phone;
-
-    cout << "person1 details:" << endl;
-    cout << person1.name << endl;
-    cout << person1.address << endl;
-    cout << person1.phone << endl;
-
-    cout << "Press the numbers to perform following actions." << endl;
-    cout << "1. Add another person detail." << endl;
-    cout << "2. Display all the information." << endl;
-
-    int question;
-    cin >> question;
-
-    switch (question) {
-        case 1:
-            addinfo();
-            break;
-        case 2:
-            cout << "1 < " << question << " till yet" << endl;
-            break;
-        default:
-            cout << "Invalid choice!" << endl;
-            break;
+            cout << "These are the details.";
+        }
     }
+int main() {
+    
+
+     ofstream file("addressbook.txt", ios::app);
+    if(file.is_open()){
+        file<< "\nPerson1 details:" << endl;
+        for(int i=0;i<people.size();i++){
+    file << "Name: " << people[i].name << endl;
+    file << "Address: " << people[i].address << endl;
+    file << "Phone: " << people[i].phone << endl;
+    }}
+    else{
+        file<<"The file is not opened to write infos.";
+    }
+    file.close();
+    int question;
+
+    do {
+        cout << "\nPress the numbers to perform following actions:\n";
+        cout << "1. Add another person detail.\n";
+        cout << "2. show all the details.\n";
+        cout << "0. Exit.\n";
+        cout << "Enter your choice: ";
+        cin >> question;
+
+        switch (question) {
+            case 1:
+                addinfo();
+                break;
+            case 2:
+                showinfo();
+                break;
+            case 0:
+                cout << "\nYou chose to exit.\n";
+                break;
+            default:
+                cout << "Invalid choice!" << endl;
+                break;
+        }
+
+    } while (question != 0);  // fixed condition (!=, not =!)
 
     return 0;
 }
